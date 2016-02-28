e1cfa2d5ed7129de26c6f165a7f59c720ce1bf5d
Romain Porte <microjoe@mailoo.org>
2016-02-28 23:53:04 +0100

    Using direct exchanger

--- reddithaum/notify.py
+++ reddithaum/notify.py
@@ -5,21 +5,22 @@ import json
 class Notifier:
     def __init__(self, addr, exchanger):
         self.exchanger = exchanger
+        self.routing_key = 'reddit'
 
         params = pika.ConnectionParameters(addr)
         self.conn = pika.BlockingConnection(params)
         self.channel = self.conn.channel()
 
-        self.channel.exchange_declare(exchange=exchanger, type='fanout')
+        self.channel.exchange_declare(exchange=exchanger, type='direct')
 
     def notify(self, obj):
 
         if 'date' in obj:
             obj['date'] = obj['date'].isoformat()
 
-        js = json.dumps(obj)
+        to_send = obj
 
         self.channel.basic_publish(
             exchange=self.exchanger,
-            routing_key='',
-            body=js)
+            routing_key=self.routing_key,
+            body=json.dumps(to_send))
